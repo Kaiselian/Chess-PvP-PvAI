7b80739a7e807ed3e4792a76be254d950270d796
Aniket "Kaiselian" Chatterjee <73754344+Kaiselian@users.noreply.github.com>
2024-08-01 23:23:46 +0530

    Screen-working

--- Chess-PvP.py
+++ Chess-PvP.py
@@ -7,10 +7,19 @@ class Main:
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode()
+        self.screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
+        pygame.display.set_caption('Chess')
 
     def mainloop(self):
-        pass
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+
+            pygame.display.update()
 
 main = Main()
 main.mainloop()
