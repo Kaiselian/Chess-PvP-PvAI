d7d38da5183ff02011c0cde2d6a6eb7bd60ea2b2
Aniket "Kaiselian" Chatterjee <73754344+Kaiselian@users.noreply.github.com>
2024-08-01 23:17:38 +0530

    Starting

---
+++ Chess-PvP.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+
+from Const import *
+
+class Main:
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode()
+
+    def mainloop(self):
+        pass
+
+main = Main()
+main.mainloop()
